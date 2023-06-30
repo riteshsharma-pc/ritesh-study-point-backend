47b10bad4926fe04dc4b89e40bafdabee79f6199
Ritesh Sharma <83967784+riteshsharma-pc@users.noreply.github.com>
2023-06-30 20:49:38 +0530

    adding cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const connectToMongo = require('./db')
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = 1000;
 connectToMongo();
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/data', require('./route/qna'))
 app.listen(process.env.PORT || port, () => {
